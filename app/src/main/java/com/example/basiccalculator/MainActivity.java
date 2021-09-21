ac3c05e75ac47e9d995133d7a624ed57b9786b50
kdani7777 <kdani7777@yahoo.com>
2021-09-21 15:25:35 -0500

    Handled edge cases

--- app/src/main/java/com/example/basiccalculator/MainActivity.java
+++ app/src/main/java/com/example/basiccalculator/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,24 +16,34 @@ public class MainActivity extends AppCompatActivity {
         EditText secondTextField = (EditText) findViewById(R.id.secondNumber);
         String firstNumber = firstTextField.getText().toString();
         String secondNumber = secondTextField.getText().toString();
-        int num1 = Integer.parseInt(firstNumber);
-        int num2 = Integer.parseInt(secondNumber);
-        int button  = view.getId();
-        //Button op = (Button) view;
-        //String operation = op.getText().toString();
+        if (firstNumber.equals("") || secondNumber.equals("")) {
+            Toast.makeText(MainActivity.this, firstNumber, Toast.LENGTH_LONG).show();
+        } else {
 
-        String result = "";
-        if (button == R.id.additionButton) {
-            result = String.valueOf(num1 + num2);
-        } else if (button == R.id.subtractionButton) {
-            result = String.valueOf(num1 - num2);
-        } else if (button == R.id.multiplicationButton) {
-            result = String.valueOf(num1 * num2);
-        } else if (button == R.id.divisionButton) {
-            result = String.valueOf(num1 / num2);
+            int num1 = Integer.parseInt(firstNumber);
+            int num2 = Integer.parseInt(secondNumber);
+            int button  = view.getId();
+            //Button op = (Button) view;
+            //String operation = op.getText().toString();
+
+            String result = "";
+            if (button == R.id.additionButton) {
+                result = String.valueOf(num1 + num2);
+            } else if (button == R.id.subtractionButton) {
+                result = String.valueOf(num1 - num2);
+            } else if (button == R.id.multiplicationButton) {
+                result = String.valueOf(num1 * num2);
+            } else if (button == R.id.divisionButton) {
+                if (num2 == 0) {
+                    result = "Error";
+                } else {
+                    result = String.valueOf(num1 / num2);
+                }
+            }
+
+            goToResult(result);
+
         }
-
-        goToResult(result);
 
     }
 
